b70de2c241270e95debc3b0b03126b7c1d066845
BassArt1 <52594689+BassArt1@users.noreply.github.com>
2025-12-29 13:32:46 +0000

    Removed Registrars, Now using deferred registers alone.

--- common/src/main/kotlin/net/bassart1/vs_levitite/register/ModBlocks.kt
+++ common/src/main/kotlin/net/bassart1/vs_levitite/register/ModBlocks.kt
@@ -1,7 +1,6 @@
 package net.bassart1.vs_levitite.register
 
 import dev.architectury.registry.registries.DeferredRegister
-import dev.architectury.registry.registries.Registrar
 import dev.architectury.registry.registries.RegistrySupplier
 import net.bassart1.vs_levitite.VSLevititeMod
 import net.minecraft.core.registries.Registries
@@ -13,9 +12,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour
 class ModBlocks {
     companion object{
         var BLOCKS: DeferredRegister<Block> = DeferredRegister.create(VSLevititeMod.MOD_ID, Registries.BLOCK)
-        var BLOCK_REGISTRAR: Registrar<Block> = BLOCKS.getRegistrar()
 
-        var LEVITITE_ENGINE: RegistrySupplier<Block> = BLOCK_REGISTRAR.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_engine"),
-            {Block(BlockBehaviour.Properties.copy(Blocks.BLAST_FURNACE))} )
+        var LEVITITE_ENGINE: RegistrySupplier<Block> = BLOCKS.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_engine")){ Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)) }
     }
 }
--- common/src/main/kotlin/net/bassart1/vs_levitite/register/ModItems.kt
+++ common/src/main/kotlin/net/bassart1/vs_levitite/register/ModItems.kt
@@ -1,7 +1,6 @@
 package net.bassart1.vs_levitite.register
 
 import dev.architectury.registry.registries.DeferredRegister
-import dev.architectury.registry.registries.Registrar
 import dev.architectury.registry.registries.RegistrySupplier
 import net.bassart1.vs_levitite.VSLevititeMod
 import net.minecraft.core.registries.Registries
@@ -12,11 +11,11 @@ import net.minecraft.world.item.Item
 class ModItems {
     companion object{
         var ITEMS: DeferredRegister<Item> = DeferredRegister.create(VSLevititeMod.MOD_ID, Registries.ITEM)
-        var ITEM_REGISTRAR: Registrar<Item> = ITEMS.getRegistrar()
 
-        var LEVITITE_CORE: RegistrySupplier<Item> = ITEM_REGISTRAR.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_core"),
+        var LEVITITE_CORE: RegistrySupplier<Item> = ITEMS.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_core"),
             { Item(Item.Properties().`arch$tab`(ModCreativeTab.LEVITITE_TAB)) } )
-        var LEVITITE_ENGINE: RegistrySupplier<Item> = ITEM_REGISTRAR.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_engine"),
+
+        var LEVITITE_ENGINE: RegistrySupplier<Item> = ITEMS.register(ResourceLocation(VSLevititeMod.MOD_ID, "levitite_engine"),
             { BlockItem(ModBlocks.LEVITITE_ENGINE.get(),Item.Properties().`arch$tab`(ModCreativeTab.LEVITITE_TAB)) } )
     }
 }
